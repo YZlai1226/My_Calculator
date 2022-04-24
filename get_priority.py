57790964d6d206c3e5eff83d9007ef08c2fc53fd
Karen <yong-zhen.lai@epitech.eu>
2022-04-24 02:58:37 +0200

    put calculator

--- get_priority.py
+++ get_priority.py
@@ -1,5 +1,10 @@
-from dataclasses import asdict
+#!/usr/bin/env python3
+
+# from dataclasses import asdict
+from tkinter import *
 import re
+
+window = Tk()
 
 class Rpn:
 
@@ -10,12 +15,18 @@ class Rpn:
         self.my_expression_list = []
         self.my_operands_list = []
         self.my_postfix_list = []
+        self.click_history = ""
         self.answer = None
 
     # def add_space(self, cal_input):
     #     cal_input = ''.join(('{}'.format(el) if el in '+-*/()' else el for el in cal_input))
     #     self.my_expression_list = cal_input.split(' ')
     #     return self.my_expression_list
+
+    def input_key(self, value):
+        self.click_history += value
+        print(self.click_history)
+        cal_input_text.set(self.click_history)
 
     def get_priority(self, operand):
         return self.priority[operand]
@@ -28,34 +39,36 @@ class Rpn:
     This function works by taking ...
     """
     def postfix_conversion(self, expression):
-        expression_start = 0
-        if expression[0] in ['+', '-']:
-            expression_start = 1
-        self.my_expression_list = re.split(r'([\+\-\*\^\(\)/])', expression[expression_start:])
-        self.my_expression_list = [element for element in self.my_expression_list if element != '']
-        if expression[0] in ['+', '-']:
-            self.my_expression_list[0] = expression[0] + self.my_expression_list[0]
-        print(f'my expression is {self.my_expression_list}')
-        # for expression in self.my_expression_list:
-        #     print(f'=> {expression}')
-        for element in self.my_expression_list:
-            if not (self.is_operand(element) or self.is_parenthesis(element)):
-                ## we have a number
-                self.my_postfix_list.append(element)
-                continue
-            if len(self.my_operands_list) == 0:
-                self.my_operands_list.append(element)
-                continue
-            if self.is_operand(element) and self.is_operand(self.my_operands_list[-1]):
-                self.pop_until_equal_or_higher_priority(element)
-            elif element == '(':
-                self.my_operands_list.append(element)
-            elif element == ')':
-                self.pop_until_close_parenthesis()
-            else: 
-                self.my_operands_list.append(element) 
-        self.my_postfix_list.append(self.my_operands_list.pop())
-        print(f'{self.my_postfix_list}')
+        print(f'the input is {expression}')
+        if len(expression) != 0:
+            expression_start = 0
+            if expression[0] in ['+', '-']:
+                expression_start = 1
+            self.my_expression_list = re.split(r'([\+\-\*\^\(\)/])', expression[expression_start:])
+            self.my_expression_list = [element for element in self.my_expression_list if element != '']
+            if expression[0] in ['+', '-']:
+                self.my_expression_list[0] = expression[0] + self.my_expression_list[0]
+            print(f'my expression is {self.my_expression_list}')
+            # for expression in self.my_expression_list:
+            #     print(f'=> {expression}')
+            for element in self.my_expression_list:
+                if not (self.is_operand(element) or self.is_parenthesis(element)):
+                    ## we have a number
+                    self.my_postfix_list.append(element)
+                    continue
+                if len(self.my_operands_list) == 0:
+                    self.my_operands_list.append(element)
+                    continue
+                if self.is_operand(element) and self.is_operand(self.my_operands_list[-1]):
+                    self.pop_until_equal_or_higher_priority(element)
+                elif element == '(':
+                    self.my_operands_list.append(element)
+                elif element == ')':
+                    self.pop_until_close_parenthesis()
+                else: 
+                    self.my_operands_list.append(element) 
+            self.my_postfix_list.append(self.my_operands_list.pop())
+            print(f'here is my final postfix : {self.my_postfix_list}')
 
     def is_operand(self, character):
         return character in self.operands
@@ -84,8 +97,13 @@ class Rpn:
         #         print(f'index is {index}')
         while len(postfix) > 1:
             postfix = self.run_whole_list(postfix)
-            print(f' in final 2222 evaluation function {postfix}')
-        return postfix
+        postfix[0] = float(postfix[0])
+        if (postfix[0]).is_integer():
+            result = int(postfix[0])
+        print(result)
+        self.click_history = ""
+        cal_input_text.set(self.click_history)
+        result_text.set(result)
 
 ## returns True if the next 3 characters are an expression
     def is_expression(self, partial_list):
@@ -98,15 +116,15 @@ class Rpn:
 
     def calculate(self, number1, number2, operand):
         if operand == '+':
-            self.answer = int(number1) + int(number2)
+            self.answer = float(number1) + float(number2)
         elif operand == '-':      
-            self.answer = int(number1) - int(number2)
+            self.answer = float(number1) - float(number2)
         elif operand == '*':    
-            self.answer = int(number1) * int(number2)
+            self.answer = float(number1) * float(number2)
         elif operand == '/':       
-            self.answer = int(number1) / int(number2)
+            self.answer = float(number1) / float(number2)
         elif operand == '^':       
-            self.answer = int(number1) ** int(number2)
+            self.answer = float(number1) ** float(number2)
 
     def run_whole_list(self, postfix):
         # for i, elem in enumerate(postfix):
@@ -128,12 +146,53 @@ class Rpn:
             i += 1
         return postfix
 
-
-
-string = '-1+12*3*((2^3+3)*4)-5'
-# string = '-1+12+3.3*((2^3+3-2+3^(4.5--2))*4)-(5)'
+    def clear(self):
+        self.click_history = ""
+        cal_input_text.set(self.click_history)
+        result_text.set("")
+    
+    def conversion_and_evaluation(self, input_string, postfix_list):
+        self.postfix_conversion(input_string)
+        self.postfix_evaluation(postfix_list)
 
 my_calculator = Rpn()
+# my_calculator.postfix_conversion(my_calculator.click_history)
+
+cal_input_text = StringVar()
+Label(window, textvariable=cal_input_text).grid(row=1, columnspan=4)
+result_text = StringVar()
+Label(window, textvariable=result_text).grid(row=2, columnspan=4)
+
+Button(window, text="Close", command=window.quit).grid(row=0, column=3)
+Button(window, text=" 0 ", command=lambda: my_calculator.input_key("0")).grid(row=6, column=0)
+Button(window, text=" 1 ", command=lambda: my_calculator.input_key("1")).grid(row=5, column=0)
+Button(window, text=" 2 ", command=lambda: my_calculator.input_key("2")).grid(row=5, column=1)
+Button(window, text=" 3 ", command=lambda: my_calculator.input_key("3")).grid(row=5, column=2)
+Button(window, text=" 4 ", command=lambda: my_calculator.input_key("4")).grid(row=4, column=0)
+Button(window, text=" 5 ", command=lambda: my_calculator.input_key("5")).grid(row=4, column=1)
+Button(window, text=" 6 ", command=lambda: my_calculator.input_key("6")).grid(row=4, column=2)
+Button(window, text=" 7 ", command=lambda: my_calculator.input_key("7")).grid(row=3, column=0)
+Button(window, text=" 8 ", command=lambda: my_calculator.input_key("8")).grid(row=3, column=1)
+Button(window, text=" 9 ", command=lambda: my_calculator.input_key("9")).grid(row=3, column=2)
+Button(window, text=" . ", command=lambda: my_calculator.input_key(".")).grid(row=7, column=0)
+Button(window, text=" ( ", command=lambda: my_calculator.input_key("(")).grid(row=7, column=1)
+Button(window, text=" ) ", command=lambda: my_calculator.input_key(")")).grid(row=7, column=2)
+Button(window, text=" + ", command=lambda: my_calculator.input_key("+")).grid(row=3, column=3)
+Button(window, text=" - ", command=lambda: my_calculator.input_key("-")).grid(row=4, column=3)
+Button(window, text=" * ", command=lambda: my_calculator.input_key("*")).grid(row=5, column=3)
+Button(window, text=" / ", command=lambda: my_calculator.input_key("/")).grid(row=6, column=3)
+Button(window, text=" ^ ", command=lambda: my_calculator.input_key("^")).grid(row=7, column=3)
+Button(window, text=" = ", command=lambda: my_calculator.conversion_and_evaluation(my_calculator.click_history, my_calculator.my_postfix_list)).grid(row=6, column=2)
+Button(window, text=" C ", command=lambda: my_calculator.clear()).grid(row=6, column=1)
+window.mainloop()
+
+# my_calculator.postfix_evaluation(my_calculator.my_postfix_list)
+
+
+# string = '-1+12*3*((2^3+3)*4)-5'
+# string = '-1+12+3.3*((2^3+3-2+3^(4.5--2))*4)-(5)'
+
+
 # print(my_calculator.get_priority('+'))
 # print(my_calculator.get_priority('^'))
 
@@ -143,10 +202,10 @@ my_calculator = Rpn()
 # print(f'1. postfix: {postfix}')
 # my_calculator.pop_until_close_parenthesis(operands, postfix)
 # my_calculator.pop_until_equal_or_higher_priority('-')
-my_calculator.postfix_conversion(string)
-print('======', my_calculator.my_postfix_list)
-my_list = ['1', '3', '+', '2', '+']
-my_calculator.postfix_evaluation(my_calculator.my_postfix_list)
+# my_calculator.postfix_conversion(string)
+# print('======', my_calculator.my_postfix_list)
+# # my_list = ['1', '3', '+', '2', '+']
+# my_calculator.postfix_evaluation(my_calculator.my_postfix_list)
 # print(my_calculator.run_whole_list(my_list))
 # print(f'2. operands: {my_calculator.my_operands_list}')
 # print(f'2. postfix: {my_calculator.my_postfix_list}')
@@ -154,5 +213,5 @@ my_calculator.postfix_evaluation(my_calculator.my_postfix_list)
 # postfix_expression = my_calculator.postfix_conversion("USERASDASDASDASD")
 # answer = my_calculator.postfix_evaluation(postfix_expression)
 
-'1+2*3*((2^2+3)*4)-5'
-'123*23+4**+5-'
+# '1+2*3*((2^2+3)*4)-5'
+# '123*23+4**+5-'
